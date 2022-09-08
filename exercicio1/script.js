633057ab4f9f0e70c6822227de170be68a95e275
marllonrp <marllonrp@yahoo.com.br>
2022-09-08 12:17:52 -0300

    Atividades diárias.

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,10 @@
-const num = //seu numero aqui
+const num = Number(prompt("Digite um número:"));
+
+if (num % 2 === 0 || num % 3 === 0) {
+    console.log("O número é divisivel por 2 ou 3");
+  } else {
+    console.log("O número não é divisel por 2 ou por 3.");
+  }
+
+
 
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,10 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+const nome = prompt("Qual é o seu nome?");
+const idade = Number(prompt("Qual é a sua idade?"));
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+nome === "José" ? console.log("Oi, Zé!") : console.log("Olá, " + nome);
+
+idade >= 18
+  ? console.log("pode tirar carteira de motorista!")
+  : console.log("Ainda não pode tirar carteira de motorista");
